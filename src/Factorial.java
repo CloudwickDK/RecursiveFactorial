127c97a5e76cff6e6247abbd8e0b9cc892fca9b5
Despoina Karna <falsedk@hotmail.com>
2016-03-06 20:52:30 +0000

    Factorial of 1 to 20

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,19 +1,38 @@
-
+/**
+ * Factorial implementation Factorials are products, indicated by an exclamation
+ * mark. For instance, "four factorial" is written as "4!" and means 1󫎿�4 =
+ * 24. In general, n! ("enn factorial") means the product of all the whole
+ * numbers from 1 to n; that is, n! = 1󫎿�...譶.
+ * 
+ * @author desp
+ *
+ */
 public class Factorial {
-	
-	public static void main(String[] args){
-		int input = 4;
-		int result = factorial(input);
-		System.out.println(result);
-		
-	
+
+	public static void main(String[] args) {
+		int input = 20;
+		/**
+		 * Calculate the factorial from 1 to 20.
+		 */
+		for (int num = 1; num <= 20; num++) {
+			long result = factorial(num);
+			System.out.println("Factorial of " + num + " is: " + result + ".");
+		}
 	}
-	static int factorial(int num){ 
-		int result = 1;	
-		if (num == 0){
+
+	/**
+	 * The factorial function
+	 * 
+	 * @param num
+	 *            The number for which we want to compute the factorial
+	 * @return the factorial of the input
+	 */
+	static long factorial(int num) {
+		long result = 1;
+		if (num == 0) {
 			return result;
 		}
-		result = factorial(num-1)* num;
+		result = factorial(num - 1) * num;// factorial(n)=factorial(n-1)*n
 		return result;
 	}
 }
